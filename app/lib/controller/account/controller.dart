620bb5f3ba4ad83ae513890988b011d31a02a6d9
Vincent <Yan-Vincent@outlook.com>
2022-03-04 13:50:07 +0800

    add model&controller

---
+++ app/lib/controller/account/controller.dart
@@ -0,0 +1,4 @@
+import './api.dart';
+
+class AccountController {
+}
---
+++ app/lib/model/account.dart
@@ -0,0 +1,17 @@
+class Account {
+  Account({this.id, this.name});
+  int? id;
+  String? name;
+
+  Account.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final data = <String, dynamic>{};
+    data['id'] = id;
+    data['name'] = name;
+    return data;
+  }
+}
