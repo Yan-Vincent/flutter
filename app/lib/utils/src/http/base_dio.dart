4c8d1b20bf00ed4bb46b6bfd34c3f3f1ffe1f9de
Vincent <Yan-Vincent@outlook.com>
2022-03-24 14:13:12 +0800

    add 拦截器

--- app/lib/utils/src/http/base_dio.dart
+++ app/lib/utils/src/http/base_dio.dart
@@ -16,6 +16,17 @@ class BaseDio with DioMixin implements Dio {
       receiveTimeout: receiveTimeout,
     );
     httpClientAdapter = DefaultHttpClientAdapter();
+    interceptors.add(InterceptorsWrapper(
+      onRequest: (options, handler) {
+        return handler.next(options);
+      },
+      onResponse: (response, handler) {
+        return handler.next(response);
+      },
+      onError: (e, handler) {
+        return handler.next(e);
+      }
+    ));
     if(kDebugMode) {
       interceptors.add(LogInterceptor(
         requestBody: true,
